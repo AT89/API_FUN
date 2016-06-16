be5df3c898d7651f54d6bdc086eb4781ce34814b
Anh Truong <anh.truong.07@cnu.edu>
2016-06-16 16:21:18 -0400

    kinda working; some images wont load since they are albums. need to make conditionals for repage loads

--- script.js
+++ script.js
@@ -1,12 +1,73 @@
-$(document).ready(function(){
+var target = '';
+var fixer = "http://accesscontrolalloworiginall.herokuapp.com/";
+var page = 2;
 
-  $("#hybridAnimals").on("click", function(){
-    console.log("clicked");
-    $.getJSON("https://www.reddit.com/r/hybridanimals/.json?jsonp=?", function(data) {
-      $.each(data.data.children, function(i,item){
-          $("<img/>").attr("src", item.data.url).appendTo("#images");
-      });
-
+var retrieveImg = function(){
+  $.getJSON(target,  function(data) {
+    $.each(data.data.children, function(i,item){
+      //if has data img
+        $("<img/>").attr("src", item.data.url).appendTo("#images");
     });
+  })
+  .fail(function(){
+    console.log("fail");
+  })
+}
+//item.data.preview.images[0].source. THIS WORKS WITH FUNNY but the hybrid animals/memes first stickied post doesnt contain a preview..so it errors. Need to add an if statement to ignore reddit post without data preview.. soo that is if = data.preview then.. do code
+
+var clearImg = function(){
+      $("#images").replaceWith("<div id='images'></div>"); //clear the current
+}
+var clearTarget = function(){
+  target = '';
+}
+
+$(document).ready(function(){
+  console.log("clicked");
+  $("#hybridAnimals").on("click", function(){
+    clearImg();
+    clearTarget();
+    target = fixer+"https://www.reddit.com/r/hybridanimals/.json?";
+    retrieveImg();
   });
-});
+
+  $("#memes").on("click", function(){
+    console.log("clicked");
+    clearImg();
+    clearTarget();
+    target = fixer+"https://www.reddit.com/r/memes/.json?";
+    retrieveImg();
+  });
+
+  $("#funny").on("click", function(){
+    console.log("clicked");
+    clearImg();
+    clearTarget();
+    target = fixer+"https://www.reddit.com/r/funny/.json?";
+    retrieveImg();
+  });
+
+//.getJSON is short for:
+// $.ajax({
+//   dataType: "json",
+//   url: url,
+//   data: data,
+//   success: success
+// });
+//
+
+//need to remove noredirect, causing some images not to be linked.. some url's are albums.. looked into the imgur api but that requires auth
+
+  $(window).scroll(function() {
+     if($(window).scrollTop() + $(window).height() == $(document).height()) {
+           $("html, body").animate({ scrollTop: 0 }, "slow");
+           target = target.replace("/.json?","/#page="+page+"/.json?");
+           retrieveImg();
+           page++;
+     }
+// use "http://accesscontrolalloworiginall.herokuapp.com/"; before to access.. overcomes API obstacle
+//      demoString = "Jack's birthday is 8/22";
+      // newString = demoString.replace("Jack","Jacques");
+  });
+
+}); //from ready
